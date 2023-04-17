d2cd7bfe3aa9278b8dcd3d56fcbe4520788d0f9a
activator95 <114784542+activator95@users.noreply.github.com>
2023-04-16 17:45:04 -0700

    added code

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # mediaApp
+
+
+## Description
+in this project we show how mongo db works 
+
+## 📝 Table of Contents
+- <a href="#description">Description</a>
+- <a href="#installation">Installation</a>
+- <a href="#usage">Usage</a>
+- <a href="#contributors">Contributors</a>
+- <a href="#license">License</a>
+- <a href="#questions">Questions</a>
+- <a href="#tests">Tests</a>
+
+## Usage
+MongoDB is an open-source, NoSQL database system that provides high performance, high availability, and easy scalability. Instead of using tables and rows as in traditional relational databases, MongoDB uses a flexible, document data model that allows for the storage of complex hierarchical data structures like JSON or BSON (Binary JSON). Documents in MongoDB are analogous to rows in a relational database, and they can have varying fields and structures.
+
+## Contributors
+Justin 
+
+## Tests
+Node.js
+MongoDB
+Mongoose
+Express
+
+## Questions
+GITHUB: https://github.com/Activator95/
+
+Contact Justinsemail12@yahoo.com
+
+## License
+This project is under MIT license
+![License](https://img.shields.io/badge/License-MIT-green.svg)
--- configuration/Connection.js
+++ configuration/Connection.js
@@ -1,11 +1,20 @@
-const { connect, connection } = require('mongoose');
-mongoose.set('strictQuery', true);
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialMedia';
+// Import the required Mongoose components
+const { connect, connection: dbConnection } = require('mongoose');
 
-connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Set strict query mode to throw errors when querying for non-existent fields
+dbConnection.set('strictQuery', true);
 
-module.exports = connection;
+// Define the MongoDB connection string, from an environment variable or a local database
+const dbURI =
+  process.env.MONGO_DB_URI || 'mongodb://localhost:27017/socialMediaApp';
+
+// Connect to the MongoDB database with the connection string and specified options
+connect(dbURI, {
+  useNewUrlParser: true, // Use the updated URL parser for the connection string
+  useUnifiedTopology: true, // Enable the unified topology for the MongoDB driver
+})
+  .then(() => console.log('Connected to MongoDB using Mongoose!'))
+  .catch((err) => console.error(`Error connecting to MongoDB: ${err.message}`));
+
+// Export the Mongoose connection object for use in other modules
+module.exports = dbConnection;
